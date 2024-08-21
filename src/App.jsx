f7760d6de9ff7a060d614e089166687fcbaba8e5
PRAVALLIKA-2301 <123469975+PRAVALLIKA-2301@users.noreply.github.com>
2024-08-21 11:04:02 +0530

    wfew

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Hero from "./Components/Hero.jsx";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router></Router>
+    </div>
+  );
+}
+
+export default App;
