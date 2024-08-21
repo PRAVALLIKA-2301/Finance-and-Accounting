8aeb5e3ce6d0ecf7873103fe2ec104a87ab2cf51
PRAVALLIKA-2301 <123469975+PRAVALLIKA-2301@users.noreply.github.com>
2024-08-21 11:33:18 +0530

    sdfg

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Hero from "./Components/Hero.jsx";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-
 function App() {
   return (
     <div className="App">
-      <Router></Router>
+      <Router>
+        <Routes>
+          <Route path="/Hero" element={<Hero />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
